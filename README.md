19029e9379d13837789d10f73ed3614f77316f2d
Scott MacDonald <kenobi@Scotts-MacBook-Pro.local>
2024-07-01 07:09:56 -0700

    Added band name generator Python Code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bandNameGenerator
 Day 1 Python bootcamp project
+
+Answer the prompts and it will generate a potential name for your new band.
---
+++ bandNameGenerator.py
@@ -0,0 +1,6 @@
+print('Welcome to the band name generator!')
+
+cityName = input('What city did you grow up in?\n')
+petName = input("What is your pet's name?\n")
+
+print('Your band name could be ' + cityName + ' ' + petName)
